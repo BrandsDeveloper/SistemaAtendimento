2935c05c94e211dc0237c83c39686926c49b7e96
Lucas Dos Santos Brandão Barbosa <76618716+BrandsDeveloper@users.noreply.github.com>
2023-10-01 16:47:47 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Faça a conexão com o banco de dados
-$servername = "35.198.52.197";
-$username = "root";
-$password = "1234";
-$dbname = "pizzaria";
+$servername = "host";
+$username = "user";
+$password = "pass";
+$dbname = "banco";
 
 // Conexão com o banco de dados
 $conn = new mysqli($servername, $username, $password, $dbname);
